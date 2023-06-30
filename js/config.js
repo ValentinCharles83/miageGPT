42377338df11aa56c8e7d25c09f634836dd42ebc
Valentin Charles <66069167+ValentinCharles83@users.noreply.github.com>
2023-07-01 00:14:32 +0200

    fix key

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = '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';
+export const API_KEY = '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';
 
 //export const API_KEY = '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';
 
